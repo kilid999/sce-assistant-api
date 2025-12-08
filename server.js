53d24f9b89a1a680ffc73f5ca4f5859e5b67f4a8
Khalid Alahmad <kilid99fr@gmail.com>
2025-12-08 20:54:19 +0300

    Add SCE assistant UI page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const OpenAI = require("openai");
-
+const path = require("path");
 dotenv.config();
 
 const app = express();
